d13f20d640b80a11a759f5651e7274043ac27679
Ömer Faruk Akduman <61577706+ofakduman@users.noreply.github.com>
2025-04-06 00:08:17 +0300

    proje dizini

---
+++ backend/src/main/java/com/stock/FinSentTradeApp/FinSentTradeApp.java
@@ -0,0 +1,17 @@
+package com.stock.FinSentTradeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSentTradeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSentTradeApp.class, args);
+
+
+
+
+	}
+
+}
